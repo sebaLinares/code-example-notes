29af862becbfcf3093cafa5fdb48af163632ac59
Sebastian Linares <slinaresl@gmail.com>
2019-07-05 11:57:31 -0400

    feat:(jwtandcookies): add cors package

--- cookies-jwt/index.js
+++ cookies-jwt/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const usuarios = require('./usuarios.json')
 const JWT = require('jsonwebtoken')
+// Parses the cookie header and populates `req.cookies` with the content
+const cookieParser = require('cookie-parser')
 
 const SECRET = 'shhh'
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.post('/auth/login', (req, res) => {
   const username = req.body.username
@@ -22,20 +25,32 @@ app.post('/auth/login', (req, res) => {
 
   const token = JWT.sign(payload, SECRET)
 
+  // name of the cookie = access_token; content of the cookie = token
   res.cookie('access_token', token, {
     // expiration date for the cookie, in secs
     maxAge: 3600,
     // js can't access the cookie
     httOnly: true
   })
-  res.send('hi')
+
+  // res.send(req.cookies.access_token)
+  res.send(cookieParser.JSONCookie(req.cookies.access_token))
 
   res.status(200).end()
 })
 
-app.use('/api/users', (req, res) => {
+app.use('/cookie', (req, res) => {
+  console.log(req.cookies['access_token'])
+})
 
-}
+app.use('/api/users/', (req, res) => {
+  const token = req.cookies.access_token
+
+  // If this fails it will throw an error
+  const decoded = JWT.verify(token, SECRET)
+
+  res.status(200).json(usuarios)
+})
 
 app.listen(3000, () => {
   console.log('node app online')
