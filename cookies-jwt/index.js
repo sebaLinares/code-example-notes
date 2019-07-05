9491ced55c1e6dcb15682643c00e0370df9b7610
Sebastian Linares <slinaresl@gmail.com>
2019-07-05 11:31:35 -0400

    feat(jwtandcookies): add cookie-parser package

--- cookies-jwt/index.js
+++ cookies-jwt/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const usuarios = require('./usuarios.json')
+const JWT = require('jsonwebtoken')
+
+const SECRET = 'shhh'
 
 app.use(express.json())
 
@@ -13,11 +16,26 @@ app.post('/auth/login', (req, res) => {
     throw new Error('username or password are not correct')
   }
 
-  res.cookie('access_token', { token: '123' })
+  const payload = {
+    _id: 'ASDFjcxxjsoidrasd'
+  }
+
+  const token = JWT.sign(payload, SECRET)
+
+  res.cookie('access_token', token, {
+    // expiration date for the cookie, in secs
+    maxAge: 3600,
+    // js can't access the cookie
+    httOnly: true
+  })
   res.send('hi')
 
   res.status(200).end()
 })
+
+app.use('/api/users', (req, res) => {
+
+}
 
 app.listen(3000, () => {
   console.log('node app online')
